f67ca6d477bbe2f1a5ed6950b4de0d7bef138161
mike-dutu <17827732572@163.com>
2026-01-28 21:34:39 +0800

    second commit

--- src/shaders.c
+++ src/shaders.c
@@ -1,42 +1,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <math.h>
 
-// //三角形
-// float vertices[] = {
-//     -0.5f, -0.5f, 0.0f,
-//     0.5f, -0.5f, 0.0f,
-//     0.0f, 0.5f, 0.0f,
-// };
-
-//矩形，用来验证EBO(IBO)
+//三角形
 float vertices[] = {
-    //第一个三角形
-    0.5f, 0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-    -0.5f, 0.5f, 0.0f,
+    //位置                      //颜色
+    -0.5f, -0.5f, 0.0f,         1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f,         0.0f, 1.0f, 0.0f,
+     0.0f, 0.5f, 0.0f,          0.0f, 0.0f, 1.0f,
 };
-
-unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3,
-};
-
 
 //顶点着色器
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+
+                                 "out vec3 ourColor;\n"
+
                                  "void main()\n"
                                  "{\n"
                                  "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "  ourColor = aColor;\n"
                                  "}\0";
 //片段着色器
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
+                                   "in vec4 vertexColor;\n"
+                                   "uniform vec4 ourColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vec4(0.2f, 0.3f, 0.4f, 1.0f);\n" 
+                                   "   FragColor = ourColor;\n" 
                                    "}\0";
 
 // 窗口大小改变时的回调函数
@@ -130,17 +124,13 @@ int main()
 
     unsigned int VBO;
     unsigned int VAO;
-    unsigned int EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
@@ -160,9 +150,16 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);         // 清空颜色缓冲区
         
         glUseProgram(shaderProgram);
+
+        //更新uniform颜色
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
 
